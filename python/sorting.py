78c63b8647cc11d23bbd22cde696c70d79c05655
Robert Rees <robert@wazoku.com>
2012-01-28 13:45:08 +0000

    Pseudo selection algo done

--- python/sorting.py
+++ python/sorting.py
@@ -1,2 +1,17 @@
 
-sorts = []
+def selection_sort(data):
+	if len(data) < 2:
+		return data
+
+	min = data[0]
+	for i in data:
+		if i < min:
+			min = i
+
+	data.remove(min)
+	sorted_data = selection_sort(data)
+	sorted_data.insert(0, min)
+	return data
+
+sorts = [selection_sort]
+
