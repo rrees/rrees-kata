3a207c47c6bdbb4cf4b348e71e9e1e6b048877e8
Robert Rees <robert@wazoku.com>
2012-03-10 16:37:39 +0000

    Working merge sort from multiple arrays

--- python/merge_sort.py
+++ python/merge_sort.py
@@ -1,4 +1,31 @@
 from itertools import chain
 
-def merge(list_of_arrays):
-	return [i for i in chain.from_iterable(list_of_arrays)]
+def merge(list_of_arrays, result = None):
+	if not result:
+		result = []
+		list_of_arrays = [sorted(arr) for arr in list_of_arrays]
+
+	if len([a for a in list_of_arrays if len(a) > 0]) == 0:
+		return result
+
+	min_head = min([arr[0] for arr in list_of_arrays if len(arr)> 0])
+	print min_head
+
+	result.append(min_head)
+
+	new_arrays = []
+	head_found = False
+	for arr in list_of_arrays:
+		if len(arr) == 0:
+			continue
+		
+		if not head_found and arr[0] == min_head:
+			new_arrays.append(arr[1:])
+			head_found = True
+		else:
+			new_arrays.append(arr)
+
+	print new_arrays
+	return merge(new_arrays, result)
+
+	
